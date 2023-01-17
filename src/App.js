638c56e2e07b296cfc00bb7ac5eac35250ebec66
jieun <balllling@naver.com>
2023-01-17 17:34:36 +0900

    2023-01-17

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { useEffect } from "react";
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
 import Header from "./components/header/Header"
-import MainPage from "./pages/MainPage"
-import { Route, Routes, Link, NavLink, Router } from "react-router-dom";
+import MainPage from "./pages/mainPage/MainPage"
+// import { Route, Routes, Link, NavLink, Router } from "react-router-dom";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
---
+++ src/pages/mainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+import MainSlider from "../../components/slider/Slider"
+
+function MainPage() {
+    return (
+        <React.Fragment>
+            <MainSlider />
+        </React.Fragment>
+    );
+}
+
+export default MainPage;
