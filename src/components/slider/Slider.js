d25006bc0c46d08d1d5965953740d034de6d0e0c
jieun <balllling92@gmail.com>
2023-01-22 01:22:31 +0900

    2023-01-22

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,24 +1,54 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import "./style.css";
+import useHover from "../useHover";
+import styled from 'styled-components';
 
 //swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper';
+
+const NeviHover = styled.button`
+  width: 42px;
+  height: 42px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 1;
+  border: none;
+  left: 20%;
+  background: url(./prev.png);
+  background-size: cover;
+  opacity: 1;
+  transition: opacity 0.3s;
+`
 
 
 function MySwiper() {
 
+  const [ref, hovering] = useHover();
+
   return (
-    <div>
+    <>
+      <NeviHover ref={ref} />
       <Swiper
-         slidesPerView={1}
-         navigation={{
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        navigation={{
           prevEl: ".button-prev-slide",
           nextEl: ".button-next-slide"
-         }}
-         modules={[Navigation]}
+        }}
+        pagination={{ type: "fraction" }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        loop={true}
+        speed={300}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
       >
         <SwiperSlide>
           <a href='#' className='bg slide1'></a>
@@ -32,14 +62,12 @@ function MySwiper() {
         <SwiperSlide>
           <a href='#' className='bg slide4'></a>
         </SwiperSlide>
-        <div className='button-prev-slide'>
-          <img src={process.env.PUBLIC_URL + '/image/prev.png'} alt='prevButton' />
-        </div>
-        <div className='button-next-slide'>
-          <img src={process.env.PUBLIC_URL + '/image/next.png'} alt='nextButton' />
+        <div>
+          {hovering ? <button className='button-prev-slide'></button> : null}
+          <button className='button-next-slide navi-share'></button>
         </div>
       </Swiper>
-    </div>
+    </>
   );
 };
 
