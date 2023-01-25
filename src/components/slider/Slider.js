1e62196747c3104eebb745c308f1c7a72aae0cfe
jieun <balllling@naver.com>
2023-01-26 06:31:45 +0900

    2023-01-26

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
-import useHover from "../useHover";
-import styled from 'styled-components';
+import { mainSliderTxt } from "../../data";
 
 //swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,29 +9,16 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination, Autoplay } from 'swiper';
 
-const NeviHover = styled.button`
-  width: 42px;
-  height: 42px;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  z-index: 1;
-  border: none;
-  left: 20%;
-  background: url(./prev.png);
-  background-size: cover;
-  opacity: 1;
-  transition: opacity 0.3s;
-`
-
 
 function MySwiper() {
 
-  const [ref, hovering] = useHover();
+  const [isHovering, setIsHovering] = useState(false);
 
   return (
-    <>
-      <NeviHover ref={ref} />
+    <div
+      onMouseOver={() => setIsHovering(true)}
+      onMouseOut={() => setIsHovering(false)}
+    >
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         slidesPerView={1}
@@ -52,23 +38,42 @@ function MySwiper() {
       >
         <SwiperSlide>
           <a href='#' className='bg slide1'></a>
+          <div className='bantxt white'>
+            <h4 className='slide-title-w'>{mainSliderTxt[0].title}</h4>
+            <h3 className='slide-subtitle-w'>{mainSliderTxt[0].subtitle}</h3>
+            <p className='content-w'>{mainSliderTxt[0].content}</p>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <a href='#' className='bg slide2'></a>
+          <div className='bantxt'>
+            <h4 className='slide-title'>{mainSliderTxt[1].title}</h4>
+            <h3 className='slide-subtitle'>{mainSliderTxt[1].subtitle}</h3>
+            <p className='content'>{mainSliderTxt[1].content}</p>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <a href='#' className='bg slide3'></a>
+          <div className='bantxt'>
+            <h4 className='slide-title'>{mainSliderTxt[2].title}</h4>
+            <h3 className='slide-subtitle'>{mainSliderTxt[2].subtitle}</h3>
+            <p className='content'>{mainSliderTxt[2].content}</p>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <a href='#' className='bg slide4'></a>
+          <div className='bantxt'>
+            <h4 className='slide-title'>{mainSliderTxt[3].title}</h4>
+            <h3 className='slide-subtitle'>{mainSliderTxt[3].subtitle}</h3>
+            <p className='content'>{mainSliderTxt[3].content}</p>
+          </div>
         </SwiperSlide>
-        <div>
-          {hovering ? <button className='button-prev-slide'></button> : null}
-          <button className='button-next-slide navi-share'></button>
-        </div>
+        <button className='button-prev-slide'></button>
+        <button className='button-next-slide'></button>
       </Swiper>
-    </>
+    </div>
   );
 };
+
 
 export default MySwiper;
