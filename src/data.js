da5d901660e27efaf550f27b75441a3acd679200
jieun <balllling@naver.com>
2023-02-17 02:27:11 +0900

    2023-02-17

--- src/data.js
+++ src/data.js
@@ -1,54 +1,101 @@
-let mainSliderTxt = [
-    {
-        id: 0,
-        title : "뿌리부터 다른 신선함",
-        subtitle : "내일 눈뜨면 도착",
-        content : "오전 7시 도착 보장"
-    },
-    {
-        id: 1,
-        title : "루트프레시에서",
-        subtitle : "가장 많이 팔린 베스트 모음",
-        content : "베스트 상품 모음"
-    },
-    {
-        id: 2,
-        title : "간편하게 뚝딱",
-        subtitle : "집에서 특별하게 즐기는 행복",
-        content : "간편 밀키트"
-    },
-    {   
-        id: 3,
-        title : "2022연말 홈파티",
-        subtitle : "'이거' 하나로 끝",
-        content : "연말 홈 패키지"
-    },
-];
 
 let mainTitle = [
+    "취향 저격 맞춤 상품",
+    "특별한 특가",
+    "11월 제철식탁 한눈에 보기",
+    "인기 카테고리 모음",
+    "오늘 수확 THE신선"
+]
+
+let mainitems = [
     {
         id: 0,
-        title : "취향 저격 맞춤 상품",
+        title: "[조은푸드]비건 채식 야채 손만두 담백한 맛 1.2kg",
+        discount_rate: "20%",
+        price: "6,786원",
+        dimmed_price: "11,800원",
     },
     {
         id: 1,
-        title : "특별한 특가",
+        title: "[맛있다]가래떡 쌀떡볶이 2인분",
+        discount_rate: "20%",
+        price: "6,375원",
+        dimmed_price: "11,800원",
     },
     {
         id: 2,
-        title : "11월 제철식탁 한눈에 보기",
+        title: "[제주농원]GAP인증 제주레몬 산지직송 500g",
+        discount_rate: "20%",
+        price: "9,900원",
+        dimmed_price: "11,800원",
     },
     {
         id: 3,
-        title : "인기 카테고리 모음",
+        title: "[365신선]동물복지 1+등급  무항생제 특란 6구",
+        discount_rate: "20%",
+        price: "3,860원",
+        dimmed_price: "11,800원",
     },
     {
         id: 4,
-        title : "오늘 수확 THE신선",
+        title: "[신선식품]단호박 생크림 스프 10팩",
+        discount_rate: "20%",
+        price: "9,440원",
+        dimmed_price: "11,800원",
     },
-];
+    {
+        id: 5,
+        title: "[프레시]카프레제 샐러드",
+        discount_rate: "10%",
+        price: "8,010원",
+        dimmed_price: "8,900원",
+    },
+    {
+        id: 6,
+        title: "[데일리팜]쫄깃한 베이글 3종",
+        discount_rate: "10%",
+        price: "2,520원",
+        dimmed_price: "2,800원",
+    },
+    {
+        id: 7,
+        title: "완숙 토마토 1kg",
+        discount_rate: "25%",
+        price: "3,750원",
+        dimmed_price: "5,000원",
+    },
+    {
+        id: 8,
+        title: "[이음]과일향 탄산수 오렌지향 250mL",
+        discount_rate: "10%",
+        price: "1,350원",
+        dimmed_price: "1,500원",
+    },
+    {
+        id: 9,
+        title: "[스테이크하우스]수비드 닭가슴살 스테이크 1팩",
+        discount_rate: "15%",
+        price: "10,200원",
+        dimmed_price: "12,000원",
+    },
+    {
+        id: 10,
+        title: "[더프레시]얼큰 김치 어묵 우동 2인분",
+        discount_rate: "35%",
+        price: "6,370원",
+        dimmed_price: "9,800원",
+    },
+    {
+        id: 11,
+        title: "[과일나라]유기농 착즙 주스 3종 250mL",
+        discount_rate: "10%",
+        price: "2,250원",
+        dimmed_price: "2,500원",
+    },
+]
+
 
 export {
-    mainSliderTxt,
     mainTitle,
-};
+    mainitems,
+}
--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,11 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./style.css"
 import MainSlider from "../../components/slider/Slider"
+import { mainTitle, mainitems} from "../../data"
+import styled from "styled-components";
+
+
+const TabButtom = styled.button`
+    height: 32px;
+    background: rgba(41, 66, 31, 0.45);
+    border: none;
+    border-radius: 30px;
+    margin: 30px 5px 0 5px;
+    padding: 0 10px;
+    font-size: 14px;
+    color: #29421f;
+`
 
 function MainPage() {
+
+    let [title, setTitle] = useState(mainTitle);
+    let [images, setImages] = useState(['/image/custom_item', '/image/sale_item', '/image/best_item']);
+    let [items, setItems] = useState(mainitems);
+
+    const spacialItem = items.filter(spacial => {
+        return spacial.id < 4;
+    })
+    const saleItem = items.filter(sale => {
+        return sale.id > 3 && sale.id < 12;
+    })
+
     return (
         <React.Fragment>
             <MainSlider />
+            <section>
+                <h2>{title[0]}</h2>
+                <div className="main-container">
+                    {spacialItem.map((spacial, i) => (
+                        <Link to="/detail">
+                            <ul className="wrap">
+                                <li>
+                                    <img style={{ borderRadius: '10px' }} src={process.env.PUBLIC_URL + images[0] + (i + 1) + '.png'} />
+                                    <div className="txtarea">
+                                        <div>
+                                            <h4 className="item-title">{spacial.title}</h4>
+                                            <span className="price">{spacial.price}</span>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </Link>))}
+                </div>
+            </section>
+
+            <div className="sub-banner">
+                <img src={process.env.PUBLIC_URL + '/image/sub_banner.png'} />
+            </div>
+
+            <section>
+                <h2>{title[1]}</h2>
+                <div>
+                    <TabButtom>타임특가</TabButtom>
+                    <TabButtom>한정특가</TabButtom>
+                    <TabButtom>루트한정특가</TabButtom>
+                </div>
+                <div className="main-container">
+                    {saleItem.map((sale, i) => (
+                        <Link to="/detail">
+                            <ul className="wrap">
+                                <li>
+                                    <img style={{ borderRadius: '10px' }} src={process.env.PUBLIC_URL + images[1] + (i + 1) + '.png'} />
+                                    <div className="txtarea">
+                                        <div className="item-price">
+                                            <h4 className="item-title">{sale.title}</h4>
+                                            <span className="discount-rate" >{sale.discount_rate}</span>
+                                            <span className="price">{sale.price}</span>
+                                            <span className="dimmed-price">{sale.dimmed_price}</span>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </Link>))}
+                </div>
+            </section>
         </React.Fragment>
     );
 }
